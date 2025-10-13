4346f013c8cd03844c538fff909be55860cf8a61
Kelven Silva <120991917+KelvenPer@users.noreply.github.com>
2025-10-13 13:54:30 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,16 @@
+// frontend/vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// Para GitHub Pages em "project page", use o nome exato do repositório:
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/fluxo_lab_automatizacao/',   // se for user/organization page ou domínio raiz, use '/'.
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
